a02f9225d7404e9a319f3bfe2c97b640c01a5313
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-05-25 13:32:45 +0900

    add file 'create_json.js' 'dialog.html'

---
+++ create_json.js
@@ -0,0 +1,41 @@
+function convertToJson() {
+  let dialog_html = HtmlService.createTemplateFromFile('dialog').evaluate();
+
+  SpreadsheetApp.getUi().showModalDialog(dialog_html, 'JSONファイルをダウンロード');
+}
+
+// getData
+function getData() {
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let maxRow = sheet.getLastRow();
+  let maxColumn = sheet.getLastColumn();
+
+  let keys = [];
+  let data = [];
+
+  for (let x = 1; x <= maxColumn; x++) {
+    keys.push(sheet.getRange(1, x).getValue());
+  }
+
+  for (let y = 2; y <= maxRow; y++) {
+    let json = {};
+    for (let x = 1; x <= maxColumn; x++) {
+      json[keys[x-1]] = sheet.getRange(y, x).getValue();
+    }
+
+    data.push(json);
+  }
+
+  return JSON.stringify(data, null, '\t');
+}
+
+// add menu
+// function onOpen() {
+//   let spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  // let entries = [{
+    // name : 'JSONで出力',
+    // functionName : 'convertToJson'
+  // }];
+  // spreadsheet.addMenu('JSON', entries);
+// };
+
